6f053d41f50bd830d94729d1fd34dad80aff1f47
chris <chris@bimspecify.com>
2016-05-18 11:21:16 +0200

    Test Commit

---
+++ helloworld/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Hello World")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Revision 1")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("Another Button")
+    End Sub
+End Class
